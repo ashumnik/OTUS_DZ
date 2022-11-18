4e84b246bed45fcae3c93ded6caa118e743a1427
ashumnik <ashumnik@list.ru>
2022-11-19 00:01:21 +0300

    delete build

--- DZ4-libraries/json-c-build/doc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aleksey/OTUS/DZ4/json-c/doc
-# Build directory: /home/aleksey/OTUS/DZ4/json-c-build/doc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
